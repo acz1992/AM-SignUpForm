1ff6ea0dcf5bde343691660d65a8df653f70c1fd
amyria3 <54778894+amyria3@users.noreply.github.com>
2022-12-16 16:46:59 +0100

    updated h1 font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,16 +6,21 @@ module.exports = {
 			padding: {
 				'1-3-1-3': '1vw 3vw 1vw 3vw',
 			  },
-		colors: {
-		'input-white': 'rgba(240, 248, 240, 1)', //also for the primary btn
-		'background-white': 'rgba(214, 229, 214, 1)',
-		'primary-green': 'rgba(44, 65, 38, 1)',
-		'default-text': 'rgba(44, 62, 65, 1)',
-		'border-grey': 'rgba(185, 215, 197, 1)'
-		},
-	},
-		borderRadius: {
-			'default': '0.4vw',
+			colors: {
+			'input-white': 'rgba(240, 248, 240, 1)', //also for the primary btn
+			'background-white': 'rgba(214, 229, 214, 1)',
+			'primary-green': 'rgba(44, 65, 38, 1)',
+			'default-text': 'rgba(44, 62, 65, 1)',
+			'border-grey': 'rgba(185, 215, 197, 1)'
+			},
+
+			borderRadius: {
+				'default': '0.8vw',
+			},
+
+			fontFamily: {
+				'quicksand': ['Quicksand', 'sans-serif'],
+			},
 		},
 	},
 	plugins: [],
